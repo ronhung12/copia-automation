241716c772b56391dea5132d3d7929daa37a7b64
Ron H <rhung@fordham.edu>
2021-08-24 17:06:06 -0400

    Delete test directory

--- test/test.js
+++
@@ -1,8 +0,0 @@
-var assert = require('assert');
-
-
-describe('sync', function() {
-    it('should return [] when the arrays are equal', function() {
-      assert.equal(sync([1,2,3],[1,2,3]), []);
-    });
-});
